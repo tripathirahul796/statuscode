9501539c00eaa626e5eab70e07a55fc4545206c8
Rahul Tripathi <tripathirahul796@gmail.com>
2024-11-16 19:41:59 +0530

    "Initial Commit"

---
+++ src/main/kotlin/com/onlinesystem/statuscode/StatusCodeApplication.kt
@@ -0,0 +1,11 @@
+package com.onlinesystem.statuscode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatusCodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatusCodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/onlinesystem/statuscode/StatusCodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onlinesystem.statuscode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatusCodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
